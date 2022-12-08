9d3ad4e2c1b7e02a4e0d94c52926ba8f5e69faf0
Rifaee123 <rifairifu2@gmail.com>
2022-12-08 13:38:21 +0530

    finished

---
+++ contact.js
@@ -0,0 +1,109 @@
+function validateName()
+{
+    let name=document.getElementById("w3lName").value;
+    let nm=null
+     nm = name.trim()
+    var spc=/^[!@#$%^&()_+\-=\[\]{};':"\\|,.<>\/?]+[A-Za-z]$/;
+    document.getElementById("w3lName").value = nm;
+    if(nm==""||name==null)
+    {
+        document.getElementById("lbname").style.display="block"
+        return false;
+    }
+    else if(!isNaN(nm[0]))
+    {
+        document.getElementById("lbname").style.display="none"
+        document.getElementById("lbname-01").style.display="block"
+        return false;
+    }
+    else if(nm.match(spc))
+    {
+        document.getElementById("lbname").style.display="none" 
+        document.getElementById("lbname-01").style.display="none"  
+        document.getElementById("lbname-02").style.display="block"
+        return false;
+    }
+    else{
+        document.getElementById("lbname").style.display="none" 
+        document.getElementById("lbname-01").style.display="none" 
+        document.getElementById("lbname-02").style.display="none"
+        return true;
+
+    }
+}
+
+
+
+
+function validateEmail(){
+
+    var val=document.getElementById("w3lSender").value;
+    var v=null
+     v = val.trim()
+    document.getElementById("w3lSender").value = v;
+    if(v==""||val==null)
+    {
+        document.getElementById("lbemail").style.display="block"
+        return false
+    }
+    else if(!isNaN(v[0]))
+    {
+        document.getElementById("lbemail-01").style.display="block"
+        return false
+    }
+
+
+    else if (!/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(v))
+  {
+    document.getElementById("lbemail-02").style.display="block"
+    return false
+  }
+   
+  else{
+        document.getElementById("lbemail").style.display="none" 
+        document.getElementById("lbemail-01").style.display="none"
+        document.getElementById("lbemail-02").style.display="none" 
+        return true
+
+    }
+}
+
+function validateSubject(){
+
+    var val=document.getElementById("w3lSubject").value;
+    var v=null
+     v = val.trim()
+    document.getElementById("w3lSubject").value = v;
+    var n=v.length
+    console.log(n)
+    if(v==""||val==null)
+    {
+        document.getElementById("lbsub").style.display="block"
+        return false
+    }
+    else{
+        
+        return true
+
+    }
+}
+function validateMessage(){
+
+
+    var val=document.getElementById("w3lMessage").value;
+    var v=null
+     v = val.trim()
+    document.getElementById("w3lMessage").value = v;
+    var n=v.length
+    console.log(n)
+    if(v==""||val==null)
+    {
+        document.getElementById("lbmes").style.display="block"
+        return false
+    }
+    else{
+        
+        return true
+
+    }
+}
